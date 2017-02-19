c29d408012295761d207d3b4766bd4a4f4da1345
Ruud van Asseldonk <dev@veniogames.com>
2017-02-19 21:17:09 +0100

    Draw text with Cairo and Freetype
    This is only in my toy C playground, not yet in Rust. But now I can
    start writing the bindings.

--- cairo/main.c
+++ cairo/main.c
@@ -1,4 +1,5 @@
 #include <cairo/cairo.h>
+#include <cairo/cairo-ft.h>
 #include <cairo/cairo-pdf.h>
 
 int main(int argc, char** argv)
@@ -16,10 +17,37 @@ int main(int argc, char** argv)
 
   cairo_stroke(cr);
 
+  double pt_size = 64.0;
+
+  // Note: FT assertions below are ignored.
+  FT_Library ft_library;
+  FT_Init_FreeType(&ft_library);
+
+  FT_Face ft_face;
+  FT_New_Face(ft_library, "/usr/share/fonts/cantarell/Cantarell-Regular.otf", 0, &ft_face);
+
+  cairo_font_face_t* font = cairo_ft_font_face_create_for_ft_face(ft_face, 0);
+  cairo_glyph_t glyphs[2];
+
+  glyphs[0].index = 73; // 'h'
+  glyphs[0].x = 128.0;
+  glyphs[0].y = 256.0;
+  glyphs[1].index = 74; // 'i'
+  glyphs[1].x = 128.0 + 64.0;
+  glyphs[1].y = 256.0;
+
+  cairo_set_font_face(cr, font);
+  cairo_set_font_size(cr, pt_size);
+  cairo_show_glyphs(cr, glyphs, 2);
+
+  cairo_font_face_destroy(font);
+
   cairo_show_page(cr);
 
   cairo_destroy(cr);
   cairo_surface_destroy(surf);
+
+  FT_Done_FreeType(ft_library);
 
   return 0;
 }
