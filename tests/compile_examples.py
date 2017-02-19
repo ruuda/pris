46f5db80c6cd828fb91be98147d2b77c7d0fac9d
Ruud van Asseldonk <dev@veniogames.com>
2017-02-19 02:30:07 +0100

    Make test script work with Python 2
    Travis does not have Python 3 installed. I could install it, but that
    would make the build slower. Just making the script a polyglot is
    simpler.

--- tests/compile_examples.py
+++ tests/compile_examples.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+# This file is deliberately both valid Python 2 and Python 3.
 
 import subprocess
 import sys
@@ -9,11 +11,11 @@ root_dir = os.path.join(os.path.dirname(__file__), '..')
 
 
 def run_pris(fname):
-    """ Run pris on the given file. Stops at a nonzero exit code. """
+    """ Run Pris on the given file. Stops at a nonzero exit code. """
     binpath = os.path.join(root_dir, 'target/debug/pris')
-    proc = subprocess.run([binpath, fname],
-                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if proc.returncode != 0:
+    proc = subprocess.Popen([binpath, fname],
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if proc.wait() != 0:
         print('FAILED {}\n'.format(fname))
         print(proc.stdout.decode('utf-8'))
         sys.exit(proc.returncode)
