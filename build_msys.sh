6c57c8caeab7a12958f8ea96bf3f433908d6156f
Ruud van Asseldonk <dev@veniogames.com>
2017-04-27 21:40:14 +0200

    Try to automate the MSYS build for Appveyor

---
+++ build_msys.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# This is the Windows build script, it is mainly responsible for ensuring
+# dependencies are installed, and invoking Cargo in the right way. Other
+# platforms do not need such a build script because we assume dependencies are
+# installed already there, and Cargo works out of the box.
+#
+# This script must be executed inside an MSYS2 shell.
+
+# Fail if any command fails.
+set -e
+
+# Install external dependencies (Cairo, Harfbuzz, etc.) and gcc and pkg-config,
+# which are required to build some of the Rust dependencies.
+
+pacman -Sy --needed           \
+  make gcc pkg-config         \
+  mingw-w64-x86_64-cairo      \
+  mingw-w64-x86_64-harfbuzz   \
+  mingw-w64-x86_64-fontconfig \
+  mingw-w64-x86_64-freetype   \
+  mingw-w64-x86_64-librsvg
+
+# Pass the library search path to rustc.
+
+export RUSTFLAGS=$(pkg-config --libs-only-L \
+  fontconfig harfbuzz librsvg-2.0 gobject-2.0 cairo freetype2)
+
+cargo build
+cargo test
