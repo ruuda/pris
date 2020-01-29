e64696f59ad3abb731853de2b8d2423e5c868753
Ruud van Asseldonk <dev@veniogames.com>
2020-01-29 21:13:08 +0100

    Add Nix-based development environment
    I wanted to run mkdocs, and apparently I no longer have it installed on
    my system. Manage it with Nix then.

---
+++ default.nix
@@ -0,0 +1,11 @@
+let
+  pkgs = import (import ./nixpkgs-pinned.nix) {};
+in
+  pkgs.buildEnv {
+    name = "pris-devenv";
+    paths = [
+      pkgs.mkdocs  # For building documentation.
+      pkgs.python3 # For running the tests.
+      pkgs.rustup  # Provides the Rust toolchain.
+    ];
+  }
